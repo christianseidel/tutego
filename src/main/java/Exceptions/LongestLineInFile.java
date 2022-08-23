a6d148fb1e86c89ab9185a005bf80323e581ef02
Christian Seidel <github@parameter5.de>
2022-08-23 18:00:03 +0200

    Exceptions

---
+++ src/main/java/Exceptions/LongestLineInFile.java
@@ -0,0 +1,47 @@
+package Exceptions;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LongestLineInFile {
+
+    public static void main(String[] args) {
+
+        Path path = Paths.get("D:\\Programmieren\\Java\\ullenboom\\tutego\\src\\main\\java\\sources\\family-names.txt");
+
+        List<String> allNames = List.of();
+
+        try {
+            allNames = Files.readAllLines(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.err.println("Die Datei konnte nicht gefunden werden.");
+        }
+
+        String longestName = "";
+        String secondLongestName = "";
+        int numberOfCharactersOfLongestName = 0;
+        int numberOfCharactersOfSecondLongestName = 0;
+
+        if (allNames.size() > 0) {
+            for (String name : allNames) {
+                if (name.length() > longestName.length()) {
+                    secondLongestName = longestName;
+                    longestName = name;
+                    numberOfCharactersOfSecondLongestName = numberOfCharactersOfLongestName;
+                    numberOfCharactersOfLongestName = name.length();
+                }
+            }
+            System.out.println("" +
+                    "Der längste Name lautet: \"" + longestName
+                    + "\" und hat " + numberOfCharactersOfLongestName + " Buchstaben."
+            + "\nDer zweitlängste Name lautet: \"" + secondLongestName
+                    + "\" und hat " + numberOfCharactersOfSecondLongestName + " Buchstaben.");
+        }
+
+
+    }
+}
